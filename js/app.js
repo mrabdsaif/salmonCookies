595e0ef6a1c96397692b09893c8321f07e7e7cca
mrabdsaif <abdullah.saif11@yahoo.com>
2020-10-13 01:10:54 +0300

    lab07 Done

--- js/app.js
+++ js/app.js
@@ -1,312 +1,158 @@
 'use strict';
 
-var hour = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 am', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm'];
+var hour = ['6:am', '7:am', '8:am', '9:am', '10:am', '11:am', '12:am', '1:pm', '2:pm', '3:pm', '4:pm', '5:pm', '6:pm', '7:pm'];
 
-var branch1 = {
-    location: 'seattle',
-    max: 65,
-    min: 23,
-    averageNumberOfCookies: 6.3,
-    numberOfCustPerHour: [],
-    cookiesPerHour: [],
-    // result: [],
-    total :0,
-    
-    // create method to generate random number for the number of customers per hour .
+// create a constructor for all Branches 
+var allBranches = [];
+console.log(allBranches);
+var lastColSum=0;
+var Branch = function (location, min, max, averageCookiesPerHour) {
+    this.location = location;
+    this.min = min;
+    this.max = max;
+    this.averageCookiesPerHour = averageCookiesPerHour;
+    this.randomCustPerHour = [];
+    this.salesPerHour = [];
+    this.dailyTotal = 0;
 
-    randomNumberOfCustomer: function (min, max) {
-        for (var j = 0; j < hour.length; j++) {
-            this.numberOfCustPerHour[j] = getRandom(min, max);
-            console.log(this.numberOfCustPerHour[j]);
-        }
-    },
-    // create a method :: to compute the sold cookies per hour --> we need to multiplay ave * numberofcu per hour  (6.3* number of customer per hour )
+    allBranches.push(this);
+}
 
-    soldCookies: function () {
+Branch.prototype.getRandom = function () {
+    for (var i = 0; i < hour.length; i++) {
+        this.randomCustPerHour.push(Math.floor(Math.random() * (this.max - this.min + 1) + this.min));
+    }
 
-        for (var i = 0; i < hour.length; i++) {
-            this.cookiesPerHour[i] = Math.ceil(this.averageNumberOfCookies * this.numberOfCustPerHour[i]);  // 
-            // this.result = `${hour[i]} : ${this.cookiesPerHour[i]} cookies`;
-            // console.log(this.result);
-            this.total += this.cookiesPerHour[i];
-        }
-        
-        console.log(this.total);
-        console.log(this.cookiesPerHour);
-    },
-    render: function () {
-        var containerE1 = document.getElementById('container');
-        var header = document.createElement('h1');
-        containerE1.appendChild(header);
-        header.textContent = this.location;
-        var ulE1 = document.createElement('ul');
-        containerE1.appendChild(ulE1);
-       
-        for (var i = 0; i < this.cookiesPerHour.length; i++) {
-            var liE1 = document.createElement('li');
-            ulE1.appendChild(liE1);
-            liE1.textContent = `${hour[i]} : ${this.cookiesPerHour[i]} Cookies`;
-            // console.log(this.cookiesPerHour[i]);
-        }
-        var liE1 = document.createElement('li');
-            ulE1.appendChild(liE1);
-            liE1.textContent = `Total : ${this.total} Cookies`  ;
-    },
-};
-
-
-branch1.randomNumberOfCustomer(23, 65);
-// console.log(branch1.randomNumberOfCustomer(23,65));
-// console.log(getRandom(23,65));
-branch1.soldCookies();
-branch1.render();
-
-// create global function to get a random number 
-
-function getRandom(min, max) {
-    var randomNumber = Math.floor((Math.random() * (max - min + 1)) + min);
-    return randomNumber;
+    // console.log(this.randomCustPerHour);
 }
 
 
-// Branch 2 
+Branch.prototype.soldCookies = function () {
+    for (var j = 0; j < hour.length; j++) {
+        this.salesPerHour.push(Math.ceil(this.randomCustPerHour[j] * this.averageCookiesPerHour));
+        this.dailyTotal += this.salesPerHour[j];
+
+    }
+    lastColSum += this.dailyTotal; 
+    console.log(this.dailyTotal);
+    // console.log(this.salesPerHour);
+}
+
+var seattle = new Branch('Seattle', 23, 65, 6.3);
+var tokyo = new Branch('Tokyo', 3, 24, 1.2);
+var dubai = new Branch('Dubai', 11, 38, 3.7);
+var paris = new Branch('Paris', 20, 38, 2.3);
+var lima = new Branch('Lima', 2, 16, 4.6);
 
 
 
-var branch2 = {
-    location: 'Tokyo',
-    max: 24,
-    min: 3,
-    averageNumberOfCookies: 1.2,
-    numberOfCustPerHour: [],
-    cookiesPerHour: [],
-    // result: [],
-    total :0,
-    // create method to generate random number for the number of customers per hour .
+var tableE = document.getElementById('table');
 
-    randomNumberOfCustomer: function (min, max) {
-        for (var j = 0; j < hour.length; j++) {
-            this.numberOfCustPerHour[j] = getRandom(min, max);
-            console.log(this.numberOfCustPerHour[j]);
+function header() {
+    var thead = document.createElement('thead');
+    tableE.appendChild(thead);
+
+    var trE = document.createElement('tr');
+    thead.appendChild(trE);
+
+    var thE = document.createElement('th');
+    trE.appendChild(thE);
+    thE.textContent = 'Branches';
+
+    for (var i = 0; i < hour.length; i++) {
+        var thE = document.createElement('th');
+        trE.appendChild(thE);
+        thE.textContent = hour[i];
+    }
+    var thE = document.createElement('th');
+    trE.appendChild(thE);
+    thE.textContent = 'Daily Total';
+
+}
+header();
+
+
+Branch.prototype.render = function () {
+
+    var tbody = document.createElement('tbody');
+    tableE.appendChild(tbody);
+
+    var trE = document.createElement('tr');
+    tbody.appendChild(trE);
+
+    var thE1 = document.createElement('th');
+    trE.appendChild(thE1);
+    thE1.textContent = this.location;
+
+    for (var i = 0; i < hour.length; i++) {
+        var tdE2 = document.createElement('td');
+        trE.appendChild(tdE2);
+        tdE2.textContent = this.salesPerHour[i];
+    }
+    var tdE3 = document.createElement('td');
+    trE.appendChild(tdE3);
+    tdE3.textContent = this.dailyTotal;
+
+}
+
+
+
+
+function footer() {
+
+    var tfoot = document.createElement('tfoot');
+    tableE.appendChild(tfoot);
+    var trE = document.createElement('tr');
+    tfoot.appendChild(trE);
+    var tdE1 = document.createElement('td');
+    trE.appendChild(tdE1);
+    tdE1.textContent = 'Total';
+
+    var hourlyTotal = [];
+    for (var i = 0; i < hour.length; i++) {
+
+        var tdE2 = document.createElement('td');
+        trE.appendChild(tdE2);
+
+        var colTotal = 0;
+        for (var j = 0; j < allBranches.length; j++) {
+
+            colTotal += (allBranches[j].salesPerHour[i]);
+            console.log(hourlyTotal);
         }
-    },
-    // create a method :: to compute the sold cookies per hour --> we need to multiplay ave * numberofcu per hour  (6.3* number of customer per hour )
 
-    soldCookies: function () {
+        hourlyTotal.push(colTotal);
 
-        for (var i = 0; i < hour.length; i++) {
-            this.cookiesPerHour[i] = Math.ceil(this.averageNumberOfCookies * this.numberOfCustPerHour[i]);  // 
-            // this.result = `${hour[i]} : ${this.cookiesPerHour[i]} cookies`;
-            // console.log(this.result);
-            this.total += this.cookiesPerHour[i];
-        }
+        tdE2.textContent = colTotal;
+    }
+    var tdElast = document.createElement('td');
+    trE.appendChild(tdElast);
+    tdElast.textContent = lastColSum;
+    
+
         
-        console.log(this.total);
-        console.log(this.cookiesPerHour);
-    },
-    render: function () {
-        var containerE1 = document.getElementById('container');
-        var header = document.createElement('h1');
-        containerE1.appendChild(header);
-        header.textContent = this.location;
-        var ulE1 = document.createElement('ul');
-        containerE1.appendChild(ulE1);
-       
-        for (var i = 0; i < this.cookiesPerHour.length; i++) {
-            var liE1 = document.createElement('li');
-            ulE1.appendChild(liE1);
-            liE1.textContent = `${hour[i]} : ${this.cookiesPerHour[i]} Cookies`;
-            // console.log(this.cookiesPerHour[i]);
-        }
-        var liE1 = document.createElement('li');
-            ulE1.appendChild(liE1);
-            liE1.textContent = `Total : ${this.total} Cookies`  ;
-    },
-};
+  
+   
+}
 
 
-branch2.randomNumberOfCustomer(3, 24);
-// console.log(branch1.randomNumberOfCustomer(23,65));
-// console.log(getRandom(23,65));
-branch2.soldCookies();
-branch2.render();
-
-// Branch 3  
 
 
-var branch3 = {
-    location: 'Dubai',
-    max: 38,
-    min: 11,
-    averageNumberOfCookies: 3.7,
-    numberOfCustPerHour: [],
-    cookiesPerHour: [],
-    // result: [],
-    total :0,
-    // create method to generate random number for the number of customers per hour .
 
-    randomNumberOfCustomer: function (min, max) {
-        for (var j = 0; j < hour.length; j++) {
-            this.numberOfCustPerHour[j] = getRandom(min, max);
-            console.log(this.numberOfCustPerHour[j]);
-        }
-    },
-    // create a method :: to compute the sold cookies per hour --> we need to multiplay ave * numberofcu per hour  (6.3* number of customer per hour )
-
-    soldCookies: function () {
-
-        for (var i = 0; i < hour.length; i++) {
-            this.cookiesPerHour[i] = Math.ceil(this.averageNumberOfCookies * this.numberOfCustPerHour[i]);  // 
-            // this.result = `${hour[i]} : ${this.cookiesPerHour[i]} cookies`;
-            // console.log(this.result);
-            this.total += this.cookiesPerHour[i];
-        }
-        
-        console.log(this.total);
-        console.log(this.cookiesPerHour);
-    },
-    render: function () {
-        var containerE1 = document.getElementById('container');
-        var header = document.createElement('h1');
-        containerE1.appendChild(header);
-        header.textContent = this.location;
-        var ulE1 = document.createElement('ul');
-        containerE1.appendChild(ulE1);
-       
-        for (var i = 0; i < this.cookiesPerHour.length; i++) {
-            var liE1 = document.createElement('li');
-            ulE1.appendChild(liE1);
-            liE1.textContent = `${hour[i]} : ${this.cookiesPerHour[i]} Cookies`;
-            // console.log(this.cookiesPerHour[i]);
-        }
-        var liE1 = document.createElement('li');
-            ulE1.appendChild(liE1);
-            liE1.textContent = `Total : ${this.total} Cookies`  ;
-    },
-};
+for (var i = 0; i < allBranches.length; i++) {
+    allBranches[i].getRandom();
+    allBranches[i].soldCookies();
+    allBranches[i].render();
+    // console.log(allBranches[i]);
+}
+footer();
 
 
-branch3.randomNumberOfCustomer(23, 65);
-// console.log(branch1.randomNumberOfCustomer(23,65));
-// console.log(getRandom(23,65));
-branch3.soldCookies();
-branch3.render();
 
 
-//Branch 4 
 
 
-var branch4 = {
-    location: 'Paris',
-    max: 38,
-    min: 20,
-    averageNumberOfCookies: 2.3,
-    numberOfCustPerHour: [],
-    cookiesPerHour: [],
-    // result: [],
-    total :0,
-    // create method to generate random number for the number of customers per hour .
-
-    randomNumberOfCustomer: function (min, max) {
-        for (var j = 0; j < hour.length; j++) {
-            this.numberOfCustPerHour[j] = getRandom(min, max);
-            console.log(this.numberOfCustPerHour[j]);
-        }
-    },
-    // create a method :: to compute the sold cookies per hour --> we need to multiplay ave * numberofcu per hour  (6.3* number of customer per hour )
-
-    soldCookies: function () {
-
-        for (var i = 0; i < hour.length; i++) {
-            this.cookiesPerHour[i] = Math.ceil(this.averageNumberOfCookies * this.numberOfCustPerHour[i]);  
-            this.total += this.cookiesPerHour[i];
-        }
-        
-        console.log(this.total);
-        console.log(this.cookiesPerHour);
-    },
-    render: function () {
-        var containerE1 = document.getElementById('container');
-        var header = document.createElement('h1');
-        containerE1.appendChild(header);
-        header.textContent = this.location;
-        var ulE1 = document.createElement('ul');
-        containerE1.appendChild(ulE1);
-       
-        for (var i = 0; i < this.cookiesPerHour.length; i++) {
-            var liE1 = document.createElement('li');
-            ulE1.appendChild(liE1);
-            liE1.textContent = `${hour[i]} : ${this.cookiesPerHour[i]} Cookies`;
-        }
-        var liE1 = document.createElement('li');
-            ulE1.appendChild(liE1);
-            liE1.textContent = `Total : ${this.total} Cookies`  ;
-    },
-};
 
 
-branch4.randomNumberOfCustomer(23, 65);
-branch4.soldCookies();
-branch4.render();
-
-
-// Branch 5 
-
-var branch5 = {
-    location: 'Lima',
-    max: 16,
-    min: 2,
-    averageNumberOfCookies: 4.6,
-    numberOfCustPerHour: [],
-    cookiesPerHour: [],
-    total :0,
-    // create method to generate random number for the number of customers per hour .
-
-    randomNumberOfCustomer: function (min, max) {
-        for (var j = 0; j < hour.length; j++) {
-            this.numberOfCustPerHour[j] = getRandom(min, max);
-            console.log(this.numberOfCustPerHour[j]);
-        }
-    },
-
-
-    // create a method :: to compute the sold cookies per hour --> we need to multiplay ave * numberofcu per hour  (6.3* number of customer per hour )
-
-    soldCookies: function () {
-
-        for (var i = 0; i < hour.length; i++) {
-            this.cookiesPerHour[i] = Math.ceil(this.averageNumberOfCookies * this.numberOfCustPerHour[i]);  // 
-            
-            this.total += this.cookiesPerHour[i];
-        }
-        
-        console.log(this.total);
-        console.log(this.cookiesPerHour);
-    },
-
-    render: function () {
-        var containerE1 = document.getElementById('container');
-        var header = document.createElement('h1');
-        containerE1.appendChild(header);
-        header.textContent = this.location;
-        var ulE1 = document.createElement('ul');
-        containerE1.appendChild(ulE1);
-       
-        for (var i = 0; i < this.cookiesPerHour.length; i++) {
-            var liE1 = document.createElement('li');
-            ulE1.appendChild(liE1);
-            liE1.textContent = `${hour[i]} : ${this.cookiesPerHour[i]} Cookies`;
-        
-        }
-        var liE1 = document.createElement('li');
-            ulE1.appendChild(liE1);
-            liE1.textContent = `Total : ${this.total} Cookies`  ;
-    },
-};
-
-
-branch5.randomNumberOfCustomer(23, 65);
-branch5.soldCookies();
-branch5.render();
 
 
